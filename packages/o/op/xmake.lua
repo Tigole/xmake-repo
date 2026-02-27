9396f0d8fcfdbd9a98c4a94718dd2d2d3365fd82
JaJaLys <sylvain.janniere@laposte.net>
2026-02-27 14:13:19 +0100

    add op

---
+++ packages/o/op/xmake.lua
@@ -0,0 +1,21 @@
+package("op")
+    set_kind("library", {headeronly = true})
+    set_description("Objects container implementation based on Wookash &amp; Anton Mikhailov discussion https://www.youtube.com/watch?v=-m7lhJ_Mzdg")
+    set_license("MIT")
+    set_homepage("https://github.com/Tigole/ObjectsPool.git")
+
+    add_urls("https://github.com/myrepo/foo.git")
+    add_versions("1.0", "5c9afe4bab70f39fc210806a84b0f279706a7509")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
