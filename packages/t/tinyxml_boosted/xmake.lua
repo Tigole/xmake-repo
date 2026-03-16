dd7504f908c89a92b184474f285a013f7a4fd255
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 14:00:31 +0100

    add tinyxml_boosted

---
+++ packages/t/tinyxml_boosted/xmake.lua
@@ -0,0 +1,21 @@
+package("op")
+    set_kind("library", {headeronly = true})
+    set_description("Objects container implementation based on Wookash &amp; Anton Mikhailov discussion https://www.youtube.com/watch?v=-m7lhJ_Mzdg")
+    set_license("MIT")
+    set_homepage("https://github.com/Tigole/TinyXML_Boosted.git")
+
+    add_urls("https://github.com/Tigole/TinyXML_Boosted.git")
+    add_versions("1.x", "9dc91a9620ed3be8ec267539600385d51c9a2272")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
