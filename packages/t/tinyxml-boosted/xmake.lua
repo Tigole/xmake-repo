da27f7fa391f8b3d22643b0a2c8d102ab3d79230
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 22:15:42 +0100

    update tinyxml-boosted

--- packages/t/tinyxml-boosted/xmake.lua
+++ packages/t/tinyxml-boosted/xmake.lua
@@ -5,7 +5,7 @@ package("tinyxml-boosted")
     set_homepage("https://github.com/Tigole/TinyXML_Boosted.git")
 
     add_urls("https://github.com/Tigole/TinyXML_Boosted.git")
-    add_versions("1.x", "f25fb25fae3b561ac92aceaba5ee71f311514013")
+    add_versions("1.x", "c8ecddf49984eefea82b89a0d5827e2283ddcfd5")
 
     on_install(function (package)
         local configs = {}
