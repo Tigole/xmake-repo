dfd167d4945a34d7290ed18c36852ed882466182
JaJaLys <sylvain.janniere@laposte.net>
2026-03-16 17:01:29 +0100

    update lys-types

--- packages/l/lys-types/xmake.lua
+++ packages/l/lys-types/xmake.lua
@@ -5,7 +5,7 @@ package("lys-types")
     set_homepage("https://github.com/Tigole/Lys-Types.git")
 
     add_urls("https://github.com/Tigole/Lys-Types.git")
-    add_versions("HEAD", "c90209f08adfe108f3988359a143c4f6b9284e21")
+    add_versions("HEAD", "0edeea8593969138600c8e0b4ce1d0c45a18361d")
 
     on_install(function (package)
         local configs = {}
