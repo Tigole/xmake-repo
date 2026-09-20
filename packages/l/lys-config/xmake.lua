654b14ecca1dad81bd7b024d2d7f6b4d60e8fd5d
JaJaLys <sylvain.janniere@laposte.net>
2026-09-20 23:00:59 +0200

    WIP

---
+++ packages/l/lys-config/xmake.lua
@@ -0,0 +1,21 @@
+package("lys-config")
+    set_kind("library", {headeronly = true})
+    set_description("Part of the Lys Framwork - Config")
+    set_license("zlib")
+    set_homepage("https://github.com/Tigole/Lys-Config.git")
+
+    add_urls("https://github.com/Tigole/Lys-Config.git")
+    add_versions("HEAD", "6a5a19d5a3610abdf9f1fc544b5073e7084cdc2c")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
---
+++ packages/l/lys-workingthread/xmake.lua
@@ -0,0 +1,21 @@
+package("lys-config")
+    set_kind("library", {headeronly = true})
+    set_description("Part of the Lys Framwork - Working Thread")
+    set_license("zlib")
+    set_homepage("https://github.com/Tigole/Lys-WorkingThread.git")
+
+    add_urls("https://github.com/Tigole/Lys-WorkingThread.git")
+    add_versions("HEAD", "4b47cd3d38bf55141766015320589760811992a9")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
