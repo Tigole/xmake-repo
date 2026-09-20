a95ae7ac4e9ea709ad2b5cc687d9f43859197ec0
JaJaLys <sylvain.janniere@laposte.net>
2026-09-20 23:46:59 +0200

    WIP

--- packages/l/lys-workingthread/xmake.lua
+++ packages/l/lys-workingthread/xmake.lua
@@ -5,7 +5,7 @@ package("lys-config")
     set_homepage("https://github.com/Tigole/Lys-WorkingThread.git")
 
     add_urls("https://github.com/Tigole/Lys-WorkingThread.git")
-    add_versions("0.0.0", "4b47cd3d38bf55141766015320589760811992a9")
+    add_versions("0.0.0", "3105ed5041bfe5b19dc6eb5119be1c102ce42f05")
 
     on_install(function (package)
         local configs = {}
